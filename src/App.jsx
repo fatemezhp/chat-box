d005f116fc6d680c309fe9896f4a3bfe687b6193
Fatemezahra Habibpour <fatemeahbibpour@gmail.com>
2025-09-09 15:05:44 +0330

    style has been completed

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
    <div>
     <div className="container">
-      <div className="chatbot-popup w-md overflow-hidden rounded-2xl shadow-md bg-white">
+      <div className="chatbot-popup md:w-md  m-auto overflow-hidden rounded-2xl shadow-md bg-white">
        { /* chatbot header */}
         <div className="chat-header bg-red-950 flex justify-between p-4">
           <div className="header-info text-white flex gap-1 items-center">
@@ -21,25 +21,34 @@ function App() {
              rounded-full bg-none h-10 w-10 hover:bg-red-700">keyboard_arrow_down</button>
         </div>
        { /* chatbot body */}
-        <div className="chat-body text-black ">
-          <div className="message bot-message">
-                <ChatBotIcon className=' fill-black'/> 
-                <p className="message-text">
+
+        <div className="chat-body h-96 p-5 text-[0.9rem]">
+          <div className="message bot-message text-black flex mb-6 ">
+                <ChatBotIcon className='flex mr-2 fill-white w-8 h-8 p-1 bg-red-950 rounded-full relative top-7'/> 
+                <p className="bot-message-text bg-red-50 rounded-[13px]  rounded-bl-none p-2 ">
                   Hi there,<br/> how can I help you today?
                 </p>
           </div>
-          <div className="message user-message">
-                <p className="message-text">
-                  chert o pert, del ro be daria mizanam, gahi be ghbl gahi be pish gahi sahra mizanam,
+          
+          <div className="message user-message text-white mb-6 ">
+                <p className="user-message-text  rounded-[13px] rounded-br-none p-2 bg-red-950 ">
+                 Lorem ipusm, something just to full this text box, belive or not.
+                 I like the way I styled this chat bot, do u as well?
                 </p>
           </div>
         </div>
        { /* chatbot footer */}
-       <div className="chat-footer">
-        <form action="#" className="chat-form">
+       <div className="chat-footer relative">
+        <form action="#" className="chat-form  w-full p-1 m-auto">
           <input type="text"
-          className='message-input' required />
-         <button className="material-symbols-rounded">arrow_upward</button>
+          className='message-input border border-gray-200 w-full 
+          rounded-full p-2 ' 
+          placeholder='Message...'
+          required 
+          />
+         <button className="material-symbols-rounded 
+         absolute right-3 bottom-2  bg-red-950 rounded-full p-1
+          text-white">arrow_upward</button>
 
         </form>
 
